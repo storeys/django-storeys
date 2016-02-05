880f7a0abb23d63d9000c919bec3d53b341dbf39
Thomas @ BeeDesk <thomas@beedesk.com>
2016-02-04 23:23:10 -0800

    Added context for storeys view.py

--- storeys/views.py
+++ storeys/views.py
@@ -1,11 +1,13 @@
 import json
-import settings
 
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 from django.views.generic.base import TemplateView
+
+EXPORTED_SETTINGS = ['STOREYS_APPS']
 
 class StoreysView(TemplateView):
     prerender_content = None
@@ -14,4 +16,7 @@ class StoreysView(TemplateView):
         context = super(StoreysView, self).get_context_data(**kwargs)
         if ('prerender_content' not in context and self.prerender_content is not None):
             context['prerender_content'] = self.prerender_content
+
+        context['settings'] = {key: getattr(settings, key) for key in EXPORTED_SETTINGS if hasattr(settings, key)}
+        context['renderer'] = 'django'
         return context
