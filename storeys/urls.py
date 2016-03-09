7cf8f0cdfddb98d50a3ed7b416778500bc185515
denti <dentiwork@yandex.ru>
2016-03-09 21:43:51 +0300

    - lilfix

--- storeys/urls.py
+++ storeys/urls.py
@@ -9,13 +9,11 @@ def urlref(*args, **kwargs):
 urlpatterns = patterns(
     '',
     url(r'^test_success_admin/', include(admin.site.urls)),
-    url(r'^app/', include('app.urls')),
     url(r'^customer/(\d+)/phone/cancel/$', TemplateView.as_view(template_name='storeys/base.html'), name='exclude2')
 )
 
 
 non_exported_urlpatterns = (
-    # urlref(module_name='app.urls'),
     urlref(module_name='admin.site.urls'),
     # urlref(name='exclude2'),
     # urlref(name='test'),
